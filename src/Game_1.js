7d038accd3f1afc06a7a38113af610d8362e7368
Thomas Askov <49984472+BigChungus420@users.noreply.github.com>
2020-05-15 11:22:12 +0200

    Made character

--- src/Game_1.js
+++ src/Game_1.js
@@ -1,18 +1,35 @@
-x = 1;
-
 function setup() {
     canvas = createCanvas(500, 500);
     canvas.parent("canvas-holder");
+    noStroke();
+
+    player = new Player(0, height/2, 20);
     
 }
 
 function draw() {
     background(235);
-    circle(x, 250, 20);
-    x += 1;
+    player.move();
+    player.character();
 }
 
 class Player {
-    constructor(x, y) {
+    constructor(xPos, yPos, radius) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.radius = radius;
+        this.color = (0, 0, 0);
+        this.xSpeed = 5;
+        this.ySpeed = 0;
+        
+    }
+
+    move() {
+        this.xPos += this.xSpeed;
+        this.yPos += this.ySpeed;
+    }
+    character() {
+        circle(this.xPos, this.yPos, this.radius, (0, 0, 0));
+        circle();
     }
 }
